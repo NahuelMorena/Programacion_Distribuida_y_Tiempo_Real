a1fde16144579f395afa23d24c4873bc8aeff4f8
Nahuel Morena <nahuel1morena1@gmail.com>
2022-12-18 01:53:50 -0300

    implement TP2 exercise 1

--- Practica 2/Ejercicio 1/client_punto1.c
+++ Practica 2/Ejercicio 1/client_punto1.c
@@ -3,14 +3,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
-#include <stdlib.h>
 #include <string.h>
-
+#include <stdlib.h>
 //Libreria utilizada para obtener los tiempos de comunicación
 #include <time.h>
+#include <math.h>
 
-//Espacio que se reservara para el buffer
-#define BUFFER_SIZE 1000
+static unsigned long sdbm(unsigned char *str);
+double standard_deviation(double *times, double mean, int loop);
 
 void error(char *msg)
 {
@@ -23,12 +23,19 @@ int main(int argc, char *argv[])
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    //cantidad de comunicaciones
+    int loop = 100;
 
-    char buffer[BUFFER_SIZE];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    //Se valida que se pasen todos los parametros requeridos
+    if (argc < 4) {
+       fprintf(stderr,"usage %s hostname port buffer_size\n", argv[0]);
        exit(0);
     }
+
+    //Asigna a la variable el tamaño que poseera el buffer
+    int buffer_size = atoi(argv[3]);
+    unsigned char buffer[buffer_size];
+
 	//TOMA EL NUMERO DE PUERTO DE LOS ARGUMENTOS
     portno = atoi(argv[2]);
 	
@@ -59,35 +66,89 @@ int main(int argc, char *argv[])
     if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
 
-    //Se completa el campo de memoria que referencia la variable buffer con el caracter 'x'
-    bzero(buffer,BUFFER_SIZE);
+    //archivo donde se concatenara la información de las trasnferencias
+    char file[10000];
+    bzero(file,10000);
+    char temp_str[2000];
 
-    for (int j=0; j < BUFFER_SIZE; j++){
+    sprintf(temp_str, "Analisis de tiempos obtenidos en mensajes de %i bytes...\n",buffer_size);
+    strcat(file, temp_str);
+
+    //Se completa el campo de memoria que referencia la variable buffer con el caracter 'x'.
+    bzero(buffer,buffer_size);
+    for (int j=0; j < (buffer_size - 1); j++){
         strcat(buffer,"x");
+    } 
+
+    //Se genera el hash del contenido del buffer
+    long hash = sdbm(buffer);
+    int original_size = strlen(buffer);
+
+    //LOOP
+    double sum_of_times = 0.0;
+    int successful_transfers = 0;
+
+    clock_t start, end;
+    double time_taken = 0.0;
+    double times[loop - 1];
+    int cont = 0;
+
+    for (int i=0; i<loop; i++){
+        start = clock();
+
+        //ENVIA UN MENSAJE AL SOCKET
+	    n = write(sockfd,buffer,buffer_size);
+        if (n < 0) 
+            error("ERROR writing to socket");
+        bzero(buffer,buffer_size);
+        
+        //ESPERA RECIBIR UNA RESPUESTA
+        cont = 0;
+        do {
+            //LEE EL MENSAJE DEL SERVIDOR
+            n = read(sockfd, &buffer[cont], buffer_size);
+            if (n < 0)
+                error("ERROR reading from socket");
+            cont += n;
+        } while (cont < buffer_size);
+
+        if (hash == sdbm(buffer) && (original_size == cont - 1)){
+            //Calculo del promedio
+            end = clock();
+            successful_transfers++;
+            time_taken = ((double) (end - start) / CLOCKS_PER_SEC) / 2.0;
+            //sum_of_times = sum_of_times + time_taken;
+            sum_of_times += time_taken;
+            times[i] = time_taken;
+        }
     }
 
-    printf("Cantidad de caracteres a enviar: %zd\n", strlen(buffer));
+    close(sockfd);
+    double mean = (sum_of_times/((double) successful_transfers));
+    sprintf(temp_str, "--->Tiempo promedio obtenido = %g segundos<---\n", mean);
+    strcat(file, temp_str);
+    sprintf(temp_str, "--->Desviación estandar = %g segundos<----\n", standard_deviation(times,mean,loop));
+    strcat(file, temp_str);
+    sprintf(temp_str, "---------------------------------------------------\n\n");
+    strcat(file, temp_str);
 
-    //Empieza a contar el tiempo de comunicación desde que se enviar el mensaje al servidor hasta que recibe una respuesta
-    clock_t start;
-    start = clock();
-
-    //ENVIA UN MENSAJE AL SOCKET
-	n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,BUFFER_SIZE);
-	
-    //ESPERA RECIBIR UNA RESPUESTA
-	n = read(sockfd,buffer,BUFFER_SIZE);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    
-    //Calcular el tiempo utilizado en segundos para mostrarlo en pantalla.
-    clock_t end;
-    end = clock();
-
-    double time_taken = ((double) end - start)/CLOCKS_PER_SEC;
-    printf("Tiempo consumido en la comunicacion del lado del cliente: %f\n", time_taken);
+    printf("%s",file);
     return 0;
 }
+
+static unsigned long sdbm(unsigned char *str){
+    unsigned long hash = 0;
+    int c;
+
+    while (c = *str++)
+        hash = c + (hash << 6) + (hash << 16) - hash;
+    return hash;
+}
+
+double standard_deviation(double *times, double mean, int loop){
+    double sum = 0;
+    for (int i = 0; i < loop; i++){
+        sum += pow((times[i] - mean),2);
+    } 
+    return sqrt(sum/loop);
+} 
--- Practica 2/Ejercicio 1/server_punto1.c
+++ Practica 2/Ejercicio 1/server_punto1.c
@@ -4,14 +4,8 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdlib.h>
 #include <string.h>
-
-//Libreria utilizada para obtener los tiempos de comunicación
-#include <time.h>
-
-//Espacio que se reservara para el buffer 
-#define BUFFER_SIZE 1000
+#include <stdlib.h>
 
 void error(char *msg)
 {
@@ -22,39 +16,43 @@ void error(char *msg)
 int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno, clilen;
-     char buffer[BUFFER_SIZE];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
 
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
+     //Se valida que se pasen todos los parametros requeridos
+     if (argc < 3) {
+          fprintf(stderr,"usage %s port_number buffer_size\n", argv[0]);
+          exit(1);
      }
 
-	 //CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXION
+     //Asigna a la variable el tamaño que poseera el buffer
+     int buffer_size = atoi(argv[2]);
+     unsigned char buffer[buffer_size];
+
+	//CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXION
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	 //AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+	//AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
 	//SOCK_STREAM - TIPO DE SOCKET 
 	
      if (sockfd < 0) 
         error("ERROR opening socket");
-
      bzero((char *) &serv_addr, sizeof(serv_addr));
      //ASIGNA EL PUERTO PASADO POR ARGUMENTO
-	 //ASIGNA LA IP EN DONDE ESCUCHA (SU PROPIA IP)
-	 portno = atoi(argv[1]);
+	//ASIGNA LA IP EN DONDE ESCUCHA (SU PROPIA IP)
+	portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
 	 
-	 //VINCULA EL FILE DESCRIPTOR CON LA DIRECCION Y EL PUERTO
-     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+	//VINCULA EL FILE DESCRIPTOR CON LA DIRECCION Y EL PUERTO
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+          sizeof(serv_addr)) < 0) 
           error("ERROR on binding");
 			  
-	 //SETEA LA CANTIDAD QUE PUEDEN ESPERAR MIENTRAS SE MANEJA UNA CONEXION		  
+	//SETEA LA CANTIDAD QUE PUEDEN ESPERAR MIENTRAS SE MANEJA UNA CONEXION		  
      listen(sockfd,5);
 	 
-	 // SE BLOQUEA A ESPERAR UNA CONEXION
+	// SE BLOQUEA A ESPERAR UNA CONEXION
      printf("Esperando por conexiones...\n");
      clilen = sizeof(cli_addr);
      newsockfd = accept(sockfd, 
@@ -64,34 +62,26 @@ int main(int argc, char *argv[])
      //DEVUELVE UN NUEVO DESCRIPTOR POR EL CUAL SE VAN A REALIZAR LAS COMUNICACIONES
 	if (newsockfd < 0) 
           error("ERROR on accept");
-     bzero(buffer,BUFFER_SIZE);
+     bzero(buffer,buffer_size);
 
+     //LOOP 100 repeticiones
+     for (int i=0; i<100; i++){
+          int cont = 0;
+          do {
+               //LEE EL MENSAJE DEL CLIENTE
+               n = read(newsockfd, &buffer[cont], buffer_size);
+               if (n < 0)
+                    error("ERROR reading from socket");
+               cont += n;
+          } while (cont < buffer_size);
 
-     int cont = 0;
-     do {
-          //LEE EL MENSAJE DEL CLIENTE
-          n = read(newsockfd,buffer,BUFFER_SIZE);
-          if (n < 0) 
-               error("ERROR reading from socket");
-          cont += n;
-     } while (cont < BUFFER_SIZE);
-	
-     printf("El mensaje es de %li bytes\n",strlen(buffer));
-	
-     clock_t start;
-     start = clock();
+	     //RESPONDE AL CLIENTE
+          n = write(newsockfd,buffer,buffer_size);
+          if (n < 0) error("ERROR writing to socket");
+          bzero(buffer,buffer_size);
+     }
 
-	//RESPONDE AL CLIENTE
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) 
-          error("ERROR writing to socket");
-
-     //Calcula el tiempo utilizado en segundos para mostrarlo en pantalla.
-     clock_t end;
-     end = clock();
-
-     double time_taken = ((double) end - start)/CLOCKS_PER_SEC;
-     printf("Tiempo consumido en la comunicación del lado del servidor: %f\n", time_taken);
+     close(newsockfd);
 
      return 0; 
 }
